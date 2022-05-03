ee4766233b783ecfe74ab6b250412a3fa7e72c4a
Carl Vitullo <carl.vitullo@gmail.com>
2022-05-03 16:54:43 -0400

    Use ts for migrations

--- packages/web/migrations/20220426042719_init.ts
+++ packages/web/migrations/20220426042719_init.ts
@@ -1,16 +1,21 @@
-function up(knex) {
+import type { Knex } from "knex";
+
+function up(knex: Knex) {
   return knex.schema
     .createTable("users", (table) => {
       table.uuid("id").primary().index().notNullable();
+      table.string("email");
+      table.string("externalId").notNullable();
+      table.string("authProvider").defaultTo("discord");
     })
     .createTable("sessions", (table) => {
       table.uuid("id").primary().index();
-      table.uuid("user_id").index().notNullable();
-      table.string("discord_refresh_token", 40).notNullable();
+      table.json("data");
+      table.datetime("expires");
     });
 }
 
-function down(knex) {
+function down(knex: Knex) {
   return knex.schema.dropTable("users").dropTable("sessions");
 }
 
