64d4b59e07e631e60d96f6782a4fdbbb79d57af4
Carl Vitullo <carl.vitullo@gmail.com>
2022-05-18 15:02:58 -0400

    Tidy

--- app/db.server.ts
+++ app/db.server.ts
@@ -4,6 +4,4 @@ import knexfile from "../knexfile";
 const environment = process.env.NODE_ENV || "development";
 const config = knexfile[environment];
 
-console.log({ config, environment });
-
 export default knex(config);
