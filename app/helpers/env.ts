4f3d339f2b717b2415fcc3155176989865ff01cd
Carl Vitullo <carl.vitullo@gmail.com>
2022-06-01 12:44:36 -0400

    Move env to helpers/env, delete channel constants

--- app/helpers/env.ts
+++ app/helpers/env.ts
@@ -2,6 +2,10 @@ const enum ENVIONMENTS {
   production = "production",
 }
 
+export const applicationKey = process.env.DISCORD_PUBLIC_KEY ?? "";
+export const applicationId = process.env.DISCORD_APP_ID ?? "";
+export const discordToken = process.env.DISCORD_HASH ?? "";
+
 export const isProd = () => process.env.ENVIRONMENT === ENVIONMENTS.production;
 
 console.log("Running as", isProd() ? "PRODUCTION" : "TEST", "environment");
