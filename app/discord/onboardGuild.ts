511fc8b7843ba03a95d58f7c2e49418c996ee0b0
Carl Vitullo <carl.vitullo@gmail.com>
2022-06-01 17:17:09 -0400

    Add basic onboarding attempts

---
+++ app/discord/onboardGuild.ts
@@ -0,0 +1,36 @@
+import type { Client, TextChannel } from "discord.js";
+import { retry } from "~/helpers/misc";
+
+import { fetchGuild } from "~/models/guilds.server";
+
+export default async (bot: Client) => {
+  // This is called any time the bot comes online, when a server becomes
+  // available after downtime, or when actually added to a new guild
+  bot.on("guildCreate", async (guild) => {
+    const appGuild = await fetchGuild(guild);
+    if (!appGuild) {
+      const welcomeMessage = `You've added automoderation! Configure the bot with the /onboard command or go to http://localhost:3000/onboard`;
+
+      const channels = await guild.channels.fetch();
+      const likelyChannels = channels.filter(
+        (c): c is TextChannel =>
+          c.isText() && (c.name.includes("mod") || c.name.includes("intro")),
+      );
+      await retry(5, async (n) => {
+        switch (n) {
+          case 0:
+            guild.systemChannel!.send(welcomeMessage);
+            return;
+          case 1:
+            guild.publicUpdatesChannel!.send(welcomeMessage);
+            return;
+          default: {
+            if (likelyChannels.size < n - 2) return;
+            likelyChannels.at(n - 2)!.send(welcomeMessage);
+            return;
+          }
+        }
+      });
+    }
+  });
+};
