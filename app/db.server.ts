f92f99c6f22063e26cfe27608088caa1a98bcaf2
Carl Vitullo <carl.vitullo@gmail.com>
2022-06-02 01:22:00 -0400

    Fix strange TS errors when including db in commands

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,7 +1,14 @@
 import knex from "knex";
-import knexfile from "../knexfile";
+import knexfile from "~/../knexfile";
 
-const environment = process.env.NODE_ENV || "development";
-const config = knexfile[environment];
+const environment = process.env.NODE_ENV || ("development" as const);
+// @ts-nocheck
+const config: {
+  client: string;
+  connection: {
+    filename: string;
+  };
+  useNullAsDefault: boolean;
+} = (knexfile as any)[environment];
 
 export default knex(config);
