vcarl/remix-discord-base

Guilds migration

migrations/20220526193702_guilds.ts
@@ -1,0 +1,15 @@
+import type { Knex } from "knex";
+
+function up(knex: Knex) {
+  return knex.schema.createTable("guilds", (table) => {
+    table.uuid("id").primary();
+    table.json("channels");
+    table.json("roles");
+  });
+}
+
+function down(knex: Knex) {
+  return knex.schema.dropTable("guilds");
+}
+
+module.exports = { up, down };

reactiflux/mod-bot

migrations/20220526193702_guilds.ts
@@ -1,0 +1,14 @@
+import type { Knex } from "knex";
+
+function up(knex: Knex) {
+  return knex.schema.createTable("guilds", (table) => {
+    table.uuid("id").primary();
+    table.json("settings");
+  });
+}
+
+function down(knex: Knex) {
+  return knex.schema.dropTable("guilds");
+}
+
+module.exports = { up, down };