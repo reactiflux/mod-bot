c49fcddfb11466862d9f428838adb6a2a5c064d1
Carl Vitullo <carl.vitullo@gmail.com>
2022-07-07 13:36:24 -0400

    Use Sentry error and release tracking

---
+++ app/helpers/sentry.server.ts
@@ -0,0 +1,24 @@
+import * as Sentry from "@sentry/node";
+// import * as Tracing from "@sentry/tracing";
+
+console.log(process.env.SENTRY_INGEST);
+
+Sentry.init({
+  dsn: process.env.SENTRY_INGEST,
+  environment: process.env.NODE_ENV,
+  integrations: [
+    // enable HTTP calls tracing
+    // new Sentry.Integrations.Http({ tracing: true }),
+    new Sentry.Integrations.OnUncaughtException(),
+    new Sentry.Integrations.OnUnhandledRejection(),
+    // enable Express.js middleware tracing
+    // new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 0.2,
+});
+
+export default Sentry;
