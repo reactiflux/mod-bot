a720798888399aaca0a2be70a4bbf107cfe1eaa9
Carl Vitullo <carl.vitullo@gmail.com>
2022-08-05 23:48:53 -0400

    MVP convene mods feature

--- app/commands/convene.tsx
+++ app/commands/convene.tsx
@@ -11,6 +11,7 @@ import { fetchSettings, SETTINGS } from "~/models/guilds.server";
 import { quoteAndEscape } from "~/helpers/discord";
 import { format } from "date-fns";
 import { useState } from "react";
+import { applyRestriction, ban, kick, timeout } from "~/models/discord.server";
 
 export const command = new ContextMenuCommandBuilder()
   .setName("Convene mods")
@@ -32,8 +33,13 @@ export const handler = async (interaction: MessageContextMenuInteraction) => {
   const { message: logMessage } = await reportUser({
     message,
     reason: ReportReasons.mod,
-    extra: `<@${interaction.user.id}> requested mods respond`,
+    extra: `‼️ <@${interaction.user.id}> requested mods respond`,
   });
+
+  if (logMessage.hasThread) {
+    return;
+  }
+
   const thread = await logMessage.startThread({
     name: `${message.author.username} mod response ${format(new Date(), "P")}`,
   });
@@ -44,33 +50,105 @@ export const handler = async (interaction: MessageContextMenuInteraction) => {
       originalMessage={message}
       onResolve={async (resolution) => {
         // TODO
-        instance.deactivate();
         switch (resolution) {
+          case resolutions.restrict:
+            reportUser({
+              reason: ReportReasons.mod,
+              message,
+              extra: "✅ Restricted",
+            });
+            await applyRestriction(message.member!);
+            message.reply(
+              "After a vote by the mods, this member has had restrictions applied to them",
+            );
+            return;
+          case resolutions.kick:
+            reportUser({
+              reason: ReportReasons.mod,
+              message,
+              extra: "✅ Kicked",
+            });
+
+            await kick(message.member!);
+            message.reply(
+              "After a vote by the mods, this member has been kicked from the server to cool off",
+            );
+            return;
+          case resolutions.ban:
+            reportUser({
+              reason: ReportReasons.mod,
+              message,
+              extra: "✅ Banned",
+            });
+
+            await ban(message.member!);
+            message.reply(
+              "After a vote by the mods, this member has been permanently banned",
+            );
+            return;
           case resolutions.informalWarning:
             reportUser({
               reason: ReportReasons.mod,
-              message: message,
-              extra: "Informal warning after mod discussion",
+              message,
+              extra: "✅ Nudge",
             });
 
             const thread = await originalChannel.threads.create({
               name: message.author.username,
               autoArchiveDuration: 60,
               // TODO: This won't work in servers that aren't at boost level 2
-              // type: "GUILD_PRIVATE_THREAD",
+              type: "GUILD_PRIVATE_THREAD",
               reason: "Private moderation thread",
             });
-            const [modRoleId] = await Promise.all([
+            const [{ moderator: modRoleId }] = await Promise.all([
               fetchSettings(message.guild!, [SETTINGS.moderator]),
               thread.members.add(message.author),
             ]);
-            await thread.send(`The <@${modRoleId}> team has determined that the following message is not okay in the community.
+            await thread.send(`The <@&${modRoleId}> team has determined that the following message is not okay in the community.
 
-This isn't a formal warning, but it concerned the moderators enough that they felt it necessary to intervene. This message was sent by a bot, but all moderators can view this thread.
+This isn't a formal warning, but your message concerned the moderators enough that they felt it necessary to intervene. This message was sent by a bot, but all moderators can view this thread and are available to discuss what concerned them.
 
   ${quoteAndEscape(message.content)}`);
             return;
+          case resolutions.formalWarning:
+            reportUser({
+              reason: ReportReasons.mod,
+              message,
+              extra: "✅ Warning",
+            });
+            message.reply(
+              `This message resulted in a formal warning from the moderators. Please review the community rules.`,
+            );
+            return;
+
+          case resolutions.okay:
+            reportUser({
+              reason: ReportReasons.mod,
+              message,
+              extra: "✅ Determined to be okay",
+            });
+
+            return;
+
+          case resolutions.track:
+            reportUser({
+              reason: ReportReasons.track,
+              message,
+            });
+            return;
+
+          case resolutions.timeout:
+            reportUser({
+              reason: ReportReasons.mod,
+              message,
+              extra: "✅ Timed out overnight",
+            });
+            timeout(message.member!);
+
+            return;
         }
+
+        instance.deactivate();
       }}
     />,
   );
@@ -80,10 +158,10 @@ This isn't a formal warning, but it concerned the moderators enough that they fe
 };
 
 const resolutions = {
+  okay: "okay",
   track: "track",
-  formalWarning: "formalWarning",
   informalWarning: "informalWarning",
-  chat: "chat",
+  formalWarning: "formalWarning",
   timeout: "timeout",
   restrict: "restrict",
   kick: "kick",
@@ -109,7 +187,7 @@ const calculateLeader = (votes: Record<Resolution, string[]>) =>
         // Boooo this cast because .entries() doesn't save key types
         accum.leader = resolution as Resolution;
         accum.voteCount = voters.length;
-        // TODO: account for ties
+        // TODO: account for ties. actually nah just require odd number of votes
       }
       return accum;
     },
@@ -132,15 +210,115 @@ const ModResponse = ({
 
   return (
     <>
-      @moderator
+      {/* @moderator TODO */}
+      {/* TODO: show vote in progress, reveal votes and unvoted mods */}
+      <Button
+        label="Okay"
+        style="success"
+        onClick={(event) => {
+          const newVotes = recordVote(votes, resolutions.okay, event.user.id);
+          setVotes(newVotes);
+
+          const { leader, voteCount } = calculateLeader(newVotes);
+          if (leader && voteCount >= votesRequired) {
+            onResolve(leader);
+          }
+        }}
+      />
+      <Button
+        label="Track"
+        onClick={(event) => {
+          const newVotes = recordVote(votes, resolutions.track, event.user.id);
+          setVotes(newVotes);
+
+          const { leader, voteCount } = calculateLeader(newVotes);
+          if (leader && voteCount >= votesRequired) {
+            onResolve(leader);
+          }
+        }}
+      />
+      <Button
+        label="Timeout"
+        onClick={(event) => {
+          const newVotes = recordVote(
+            votes,
+            resolutions.timeout,
+            event.user.id,
+          );
+          setVotes(newVotes);
+
+          const { leader, voteCount } = calculateLeader(newVotes);
+          if (leader && voteCount >= votesRequired) {
+            onResolve(leader);
+          }
+        }}
+      />
       <Button
         label="Informal Warning"
+        style="primary"
         onClick={(event) => {
           const newVotes = recordVote(
             votes,
             resolutions.informalWarning,
             event.user.id,
           );
+          setVotes(newVotes);
+
+          const { leader, voteCount } = calculateLeader(newVotes);
+          if (leader && voteCount >= votesRequired) {
+            onResolve(leader);
+          }
+        }}
+      />
+      <Button
+        label="Formal Warning"
+        onClick={(event) => {
+          const newVotes = recordVote(
+            votes,
+            resolutions.formalWarning,
+            event.user.id,
+          );
+          setVotes(newVotes);
+
+          const { leader, voteCount } = calculateLeader(newVotes);
+          if (leader && voteCount >= votesRequired) {
+            onResolve(leader);
+          }
+        }}
+      />
+      <Button
+        label="Restrict"
+        onClick={(event) => {
+          const newVotes = recordVote(
+            votes,
+            resolutions.restrict,
+            event.user.id,
+          );
+          setVotes(newVotes);
+
+          const { leader, voteCount } = calculateLeader(newVotes);
+          if (leader && voteCount >= votesRequired) {
+            onResolve(leader);
+          }
+        }}
+      />
+      <Button
+        label="Kick"
+        onClick={(event) => {
+          const newVotes = recordVote(votes, resolutions.kick, event.user.id);
+          setVotes(newVotes);
+
+          const { leader, voteCount } = calculateLeader(newVotes);
+          if (leader && voteCount >= votesRequired) {
+            onResolve(leader);
+          }
+        }}
+      />
+      <Button
+        label="Ban"
+        style="danger"
+        onClick={(event) => {
+          const newVotes = recordVote(votes, resolutions.ban, event.user.id);
           setVotes(newVotes);
 
           const { leader, voteCount } = calculateLeader(newVotes);
