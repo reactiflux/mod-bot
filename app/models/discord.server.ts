b27d581d235f11df6ffb24010b7fbb18b936ca68
Carl Vitullo <carl.vitullo@gmail.com>
2022-08-05 23:47:40 -0400

    Add discord model methods for moderating members

--- app/models/discord.server.ts
+++ app/models/discord.server.ts
@@ -1,4 +1,6 @@
+import type { GuildMember } from "discord.js";
 import type { AccessToken } from "simple-oauth2";
+import { fetchSettings, SETTINGS } from "./guilds.server";
 
 export interface DiscordUserInfo {
   id: string;
@@ -40,3 +42,28 @@ export async function fetchUser(access: AccessToken): Promise<DiscordUserInfo> {
     avatar,
   };
 }
+
+export const applyRestriction = async (member: GuildMember) => {
+  const { restricted } = await fetchSettings(member.guild!, [
+    SETTINGS.restricted,
+  ]);
+  if (!restricted) {
+    throw new Error(
+      "Tried to restrict with no restricted role configured. This is likely a development error.",
+    );
+  }
+  return member.roles.add((await member.guild!.roles.fetch(restricted))!);
+};
+
+export const kick = async (member: GuildMember) => {
+  return member.kick();
+};
+
+export const ban = async (member: GuildMember) => {
+  return member.ban();
+};
+
+const OVERNIGHT = 1000 * 60 * 60 * 20;
+export const timeout = async (member: GuildMember) => {
+  return member.timeout(OVERNIGHT);
+};
