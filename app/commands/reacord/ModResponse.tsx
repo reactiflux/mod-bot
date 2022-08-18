8bbdd73c0ab271dd67a9d3ad0e44b28f3ea338eb
Carl Vitullo <carl.vitullo@gmail.com>
2022-08-17 22:29:33 -0400

    Tighten up abstractions for convene mods command

---
+++ app/commands/reacord/ModResponse.tsx
@@ -0,0 +1,57 @@
+import { Button } from "reacord";
+
+import type { Resolution } from "~/helpers/modResponse";
+import { resolutions, useVotes } from "~/helpers/modResponse";
+
+// TODO const VOTES_TO_APPROVE = 3
+const VOTES_TO_APPROVE = 1;
+
+export const ModResponse = ({
+  votesRequired = VOTES_TO_APPROVE,
+  onResolve,
+  modRoleId,
+}: {
+  votesRequired?: number;
+  onResolve: (result: Resolution) => void;
+  modRoleId: string;
+}) => {
+  const { votes, recordVote } = useVotes();
+
+  const renderButton = (
+    votes: Record<Resolution, string[]>,
+    resolution: Resolution,
+    label: string,
+    style: "secondary" | "primary" | "success" | "danger" = "secondary",
+  ) => (
+    <Button
+      label={label}
+      style={style}
+      onClick={(event) => {
+        const { leader, voteCount } = recordVote(
+          votes,
+          resolution,
+          event.user.id,
+        );
+
+        if (leader && voteCount >= votesRequired) {
+          onResolve(leader);
+        }
+      }}
+    />
+  );
+
+  return (
+    <>
+      {`<@&${modRoleId} >`}
+      {/* TODO: show vote in progress, reveal votes and unvoted mods */}
+      {renderButton(votes, resolutions.okay, "Okay", "success")}
+      {renderButton(votes, resolutions.track, "Track")}
+      {renderButton(votes, resolutions.timeout, "Timeout")}
+      {renderButton(votes, resolutions.nudge, "Nudge", "primary")}
+      {renderButton(votes, resolutions.warning, "Formal Warning")}
+      {renderButton(votes, resolutions.restrict, "Restrict")}
+      {renderButton(votes, resolutions.kick, "Kick")}
+      {renderButton(votes, resolutions.ban, "Ban", "danger")}
+    </>
+  );
+};
