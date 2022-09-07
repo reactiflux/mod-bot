3d07b1f99d8b3bd7a9371b6a9fe8383130b94bf4
Carl Vitullo <carl.vitullo@gmail.com>
2022-09-07 17:06:19 -0400

    Improve env loading behavior

--- app/helpers/env.ts
+++ app/helpers/env.ts
@@ -2,10 +2,22 @@ const enum ENVIONMENTS {
   production = "production",
 }
 
-export const applicationKey = process.env.DISCORD_PUBLIC_KEY ?? "";
-export const applicationId = process.env.DISCORD_APP_ID ?? "";
-export const discordToken = process.env.DISCORD_HASH ?? "";
+let ok = true;
+const getEnv = (key: string, optional = false) => {
+  const value = process.env[key];
+  if (!value && !optional) {
+    console.log(`Add a ${key} value to .env`);
+    ok = false;
+    return "";
+  }
+  return value ?? "";
+};
 
 export const isProd = () => process.env.ENVIRONMENT === ENVIONMENTS.production;
-
 console.log("Running as", isProd() ? "PRODUCTION" : "TEST", "environment");
+
+export const applicationKey = getEnv("DISCORD_PUBLIC_KEY");
+export const applicationId = getEnv("DISCORD_APP_ID");
+export const discordToken = getEnv("DISCORD_HASH");
+
+if (!ok) throw new Error("Environment misconfigured");
