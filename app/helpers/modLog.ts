c7c366f6cb8851cbda62c7c76b7ed93e059660b5
Carl Vitullo <carl.vitullo@gmail.com>
2022-10-25 14:13:56 -0400

    Remove unused log types and simplify logic
    WOW this was a substantial reduction in logic. A lot of this code was supporting removed use cases, by removing the need to ping mod _sometimes_ we can strip out a ton of the different code paths

--- app/helpers/modLog.ts
+++ app/helpers/modLog.ts
@@ -1,6 +1,5 @@
 import type {
   Message,
-  Role,
   TextChannel,
   MessageCreateOptions,
   User,
@@ -19,8 +18,6 @@ import { format, formatDistanceToNowStrict } from "date-fns";
 export const enum ReportReasons {
   anonReport = "anonReport",
   track = "track",
-  userWarn = "userWarn",
-  userDelete = "userDelete",
   mod = "mod",
   spam = "spam",
   ping = "ping",
@@ -28,8 +25,6 @@ export const enum ReportReasons {
 const ReadableReasons: Record<ReportReasons, string> = {
   [ReportReasons.anonReport]: "Reported anonymously",
   [ReportReasons.track]: "Tracked",
-  [ReportReasons.userWarn]: "Met 👎 threshold",
-  [ReportReasons.userDelete]: "Met 👎 deletion threshold",
   [ReportReasons.mod]: "Moderators convened",
   [ReportReasons.spam]: "Detected as spam",
   [ReportReasons.ping]: "Pinged everyone",
@@ -107,93 +102,33 @@ const constructLog = async ({
 }: Pick<Report, "extra" | "staff"> & {
   logs: Report[];
 }): Promise<MessageCreateOptions> => {
-  const { guild } = logs[0].message;
   const lastReport = logs.at(-1)!;
-  const { moderator: moderatorId } = await fetchSettings(guild!, [
-    SETTINGS.moderator,
-  ]);
 
-  const staffRole = (await guild!.roles.fetch(moderatorId)) as Role;
-
-  const modAlert = `<@${staffRole.id}>`;
-  const preface = `<@${lastReport.message.author.id}> (${
-    lastReport.message.author.username
-  }) warned ${logs.length} times:
-${logs
-  .map(
-    ({ message, reason, staff }) =>
-      `${formatDistanceToNowStrict(lastReport.message.createdAt)} ago in <#${
-        message.channel.id
-      }> ${ReadableReasons[reason]}${
-        staff ? `, by ${staff.username}` : ""
-      }: ${constructDiscordLink(message)} on ${format(
-        lastReport.message.createdAt,
-        "Pp 'GMT'x",
-      )}`,
-  )
-  .join("\n")}`.trim();
+  const reports = logs
+    .map(
+      ({ message, reason, staff }) =>
+        `${formatDistanceToNowStrict(lastReport.message.createdAt)} ago in <#${
+          message.channel.id
+        }> ${ReadableReasons[reason]}${
+          staff ? `, by ${staff.username}` : ""
+        }: ${constructDiscordLink(message)} on ${format(
+          lastReport.message.createdAt,
+          "Pp 'GMT'x",
+        )}`,
+    )
+    .join("\n")
+    .trim();
+  const preface = `<@${lastReport.message.author.id}> (${lastReport.message.author.username}) warned ${logs.length} times:`;
   const extra = origExtra ? `\n${origExtra}\n` : "";
 
   const reportedMessage = quoteAndEscape(lastReport.message.content).trim();
   const attachments = describeAttachments(lastReport.message.attachments);
 
-  const reasons = logs.map(({ reason }) => reason);
-  const outputFormat = getLogFormat(reasons);
-  switch (outputFormat) {
-    case ReportReasons.mod:
-      return {
-        content: `${preface}:${extra}
-${reportedMessage}`,
-        embeds: [{ description: `${attachments ? `\n\n${attachments}` : ""}` }],
-      };
-
-    case ReportReasons.track:
-      return {
-        content: `${preface}
+  return {
+    content: `${preface}
+${reports}
 ${extra}
 ${reportedMessage}`,
-        embeds: [{ description: `${attachments ? `\n\n${attachments}` : ""}` }],
-      };
-
-    case ReportReasons.userWarn:
-    case ReportReasons.userDelete:
-      return {
-        content: `${modAlert} – ${preface}
-${extra}
-${reportedMessage}`,
-        embeds: [{ description: `${attachments ? `\n\n${attachments}` : ""}` }],
-      };
-
-    case ReportReasons.spam:
-    case ReportReasons.ping:
-    case ReportReasons.anonReport:
-      return {
-        content: `${preface}
-${extra}
-${reportedMessage}`,
-        embeds: [{ description: `${attachments ? `\n\n${attachments}` : ""}` }],
-      };
-  }
+    embeds: [{ description: `${attachments ? `\n\n${attachments}` : ""}` }],
+  };
 };
-
-const reportScores = {
-  [ReportReasons.anonReport]: 1,
-  [ReportReasons.track]: 3,
-  [ReportReasons.userWarn]: 1,
-  [ReportReasons.userDelete]: 1,
-  [ReportReasons.mod]: 5,
-  [ReportReasons.spam]: 1,
-  [ReportReasons.ping]: 1,
-};
-// Determine what format to use when sending a message. When a single message can
-// be reported multiple times, we'd prefer that some types of reports have more
-// weight over what format is used. For instance, if a message is tracked by
-// moderators, then reported anonymously, we'd prefer to render it as a "tracked
-// by moderators" message because it has more information.
-const getLogFormat = (reports: ReportReasons[]): ReportReasons =>
-  reports.reduce((mainType, report) => {
-    if (reportScores[report] >= reportScores[mainType]) {
-      return report;
-    }
-    return mainType;
-  }, reports[0]);
