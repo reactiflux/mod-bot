070d584da1cdfd2a3c92a448fdd74c26bb0d7911
Carl Vitullo <carl.vitullo@gmail.com>
2024-11-21 16:46:36 -0500

    Working ticket system

--- app/commands/setupTickets.ts
+++ app/commands/setupTickets.ts
@@ -1,6 +1,7 @@
 import type {
   APIInteraction,
   APIInteractionResponseChannelMessageWithSource,
+  APIModalSubmitInteraction,
   ChatInputCommandInteraction,
 } from "discord.js";
 import {
@@ -23,8 +24,17 @@ import { ChannelType, Routes } from "discord-api-types/v10";
 import { discordToken } from "~/helpers/env";
 import { SETTINGS, fetchSettings } from "~/models/guilds.server";
 import { format } from "date-fns";
+import { MessageComponentTypes, TextStyleTypes } from "discord-interactions";
+import { quoteMessageContent } from "~/helpers/discord";
 
 const rest = new REST({ version: "10" }).setToken(discordToken);
+
+const isModalInteraction = (body: any): body is APIModalSubmitInteraction => {
+  return (
+    body.message.interaction_metadata.type === 2 &&
+    body.data.custom_id === "modal-open-ticket"
+  );
+};
 
 export const command = new SlashCommandBuilder()
   .setName("tickets-channel")
@@ -38,52 +48,90 @@ export const command = new SlashCommandBuilder()
 export const webserver: RequestHandler = async (req, res, next) => {
   const body = req.body as APIInteraction;
   // @ts-expect-error because apparently custom_id types are broken
-  if (body.data.component_type !== 2 || body.data.custom_id !== "open-ticket") {
+  console.log("hook:", body.data.component_type, body.data.custom_id);
+  // @ts-expect-error because apparently custom_id types are broken
+  if (body.data.component_type === 2 && body.data.custom_id === "open-ticket") {
+    res.send({
+      type: InteractionResponseType.Modal,
+      data: {
+        custom_id: "modal-open-ticket",
+        title: "What do you need from the moderators?",
+        components: [
+          {
+            type: MessageComponentTypes.ACTION_ROW,
+            components: [
+              {
+                type: MessageComponentTypes.INPUT_TEXT,
+                custom_id: "concern",
+                label: "Concern",
+                style: TextStyleTypes.PARAGRAPH,
+                min_length: 30,
+                max_length: 500,
+                required: true,
+              },
+            ],
+          },
+        ],
+      },
+    });
     return;
   }
-  if (
-    !body.channel ||
-    !body.message ||
-    !body.message.interaction_metadata?.user
-  ) {
-    console.error("ticket creation error", JSON.stringify(req.body));
+  if (isModalInteraction(body)) {
+    if (
+      !body.channel ||
+      !body.message ||
+      !body.message.interaction_metadata?.user ||
+      !body.data?.components[0].components[0].value
+    ) {
+      console.error("ticket creation error", JSON.stringify(req.body));
+      res.send({
+        type: InteractionResponseType.ChannelMessageWithSource,
+        data: {
+          content: "Something went wrong while creating a ticket",
+          flags: MessageFlags.Ephemeral,
+        },
+      } as APIInteractionResponseChannelMessageWithSource);
+      return;
+    }
+
+    const { [SETTINGS.moderator]: mod } = await fetchSettings(
+      // @ts-expect-error because this shouldn't have used a Guild instance but
+      // it's a lot to refactor
+      { id: body.guild_id },
+      [SETTINGS.moderator],
+    );
+    const thread = (await rest.post(Routes.threads(body.channel.id), {
+      body: {
+        name: `${body.message.interaction_metadata.user.username} – ${format(
+          new Date(),
+          "PP kk:mmX",
+        )}`,
+        auto_archive_duration: 60 * 24 * 7,
+        type: ChannelType.PrivateThread,
+      } as RESTPostAPIChannelThreadsJSONBody,
+    })) as RESTPostAPIChannelThreadsResult;
+    await rest.post(Routes.channelMessages(thread.id), {
+      body: {
+        content: `<@${body.message.interaction_metadata.user.id}>, this is a private space only visible to the <@&${mod}> role.`,
+      } as RESTPostAPIChannelMessageJSONBody,
+    });
+    await rest.post(Routes.channelMessages(thread.id), {
+      body: {
+        content: `${quoteMessageContent(
+          body.data?.components[0].components[0].value,
+        )}`,
+      },
+    });
+
     res.send({
       type: InteractionResponseType.ChannelMessageWithSource,
       data: {
-        content: "Something went wrong while creating a ticket",
+        content: `A private thread with the moderation team has been opened for you: <#${thread.id}>`,
         flags: MessageFlags.Ephemeral,
       },
     } as APIInteractionResponseChannelMessageWithSource);
     return;
   }
-  const { [SETTINGS.moderator]: mod } = await fetchSettings(
-    // @ts-ignore
-    { id: body.guild_id },
-    [SETTINGS.moderator],
-  );
-  const thread = (await rest.post(Routes.threads(body.channel.id), {
-    body: {
-      name: `${body.message.interaction_metadata.user.username} – ${format(
-        new Date(),
-        "PP kk:mmX",
-      )}`,
-      auto_archive_duration: 60 * 24 * 7,
-      type: ChannelType.PrivateThread,
-    } as RESTPostAPIChannelThreadsJSONBody,
-  })) as RESTPostAPIChannelThreadsResult;
-  await rest.post(Routes.channelMessages(thread.id), {
-    body: {
-      content: `<@${body.message.interaction_metadata.user.id}>, this is a private space only visible to the <@&${mod}> role. Please describe what you need the moderators.`,
-    } as RESTPostAPIChannelMessageJSONBody,
-  });
-
-  res.send({
-    type: InteractionResponseType.ChannelMessageWithSource,
-    data: {
-      content: `A private thread with the moderation team has been opened for you: <#${thread.id}>`,
-      flags: MessageFlags.Ephemeral,
-    },
-  } as APIInteractionResponseChannelMessageWithSource);
 };
 
 export const handler = async (interaction: ChatInputCommandInteraction) => {
