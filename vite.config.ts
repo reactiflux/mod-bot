374bc36238d324f757fac1ba5303d589ba991dd6
Carl Vitullo <carl.vitullo@gmail.com>
2024-12-23 16:34:30 -0500

    Migrate to remix routes future flag

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { createRoutesFromFolders } from "@remix-run/v1-route-convention";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -18,10 +17,6 @@ export default defineConfig({
   plugins: [
     tsconfigPaths(),
     remix({
-      routes(defineRoutes) {
-        // uses the v1 convention, works in v1.15+ and v2
-        return createRoutesFromFolders(defineRoutes);
-      },
       ignoredRouteFiles: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
       future: {
         unstable_optimizeDeps: true,
@@ -30,6 +25,7 @@ export default defineConfig({
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
         v3_throwAbortReason: true,
+        v3_routeConfig: true,
       },
     }),
   ],
