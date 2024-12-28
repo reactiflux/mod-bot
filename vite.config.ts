e926b9288c5f9884766d990581710b3b99b0f413
Carl Vitullo <carl.vitullo@gmail.com>
2024-12-28 15:04:37 -0500

    fixup! Rely on Node Subpath Imports, not TS paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ isSsrBuild }) => ({
   build: {
     rollupOptions: isSsrBuild ? { input: "./app/server.ts" } : undefined,
   },
   server: { port: 3000, origin: "localhost:3000" },
-  plugins: [tsconfigPaths(), reactRouter()],
+  plugins: [reactRouter()],
 }));
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,10 +3,9 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   test: {
     globals: true,
     environment: "happy-dom",
