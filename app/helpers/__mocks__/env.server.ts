e435af1586093119f5b869cffd76065acf9714a7
Carl Vitullo <carl.vitullo@gmail.com>
2024-12-28 03:08:21 -0500

    Fix borked test

---
+++ app/helpers/__mocks__/env.server.ts
@@ -0,0 +1,10 @@
+export const isProd = () => false;
+
+export const databaseUrl = "DATABASE_URL";
+export const sessionSecret = "SESSION_SECRET";
+
+export const applicationKey = "DISCORD_PUBLIC_KEY";
+export const discordSecret = "DISCORD_SECRET";
+export const applicationId = "DISCORD_APP_ID";
+export const discordToken = "DISCORD_HASH";
+export const testGuild = "DISCORD_TEST_GUILD";
