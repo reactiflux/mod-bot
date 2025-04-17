2a9881cd958f94299c333cb12fa57d67866cff94
Carl Vitullo <carl.vitullo@gmail.com>
2025-04-17 14:29:16 -0400

    Update public warning copy

--- app/helpers/escalate.tsx
+++ app/helpers/escalate.tsx
@@ -148,14 +148,14 @@ export async function escalate(
               fetchSettings(reportedMessage.guild!, [SETTINGS.moderator]),
               thread.members.add(reportedMessage.author),
             ]);
-            await thread.send(`The <@&${modRoleId}> team has determined that the following message is not okay in the community.
+            await thread.send(`The <@& ${modRoleId}> team has determined that the following message is not okay in the community.
 
 Your message concerned the moderators enough that they felt it necessary to intervene. This message was sent by a bot, but all moderators can view this thread and are available to discuss what concerned them.
 
 ${quoteAndEscape(reportedMessage.content)}`);
 
             reportedMessage.reply(
-              `This message resulted in a formal warning from the moderators. Please review the community rules.`,
+              `This user has been formally warned by the moderators. Please review the community rules.`,
             );
             return;
           }
