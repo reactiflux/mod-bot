4827542c42d050846e70bb536a88493f918362a1
DanielFGray <danielfgray@gmail.com>
2025-06-10 17:46:38 -0500

    add tooltip for radial graph

--- app/routes/sh-user.tsx
+++ app/routes/sh-user.tsx
@@ -247,6 +247,7 @@ export default function UserProfile({
           <PolarGrid />
           <PolarAngleAxis dataKey="category" />
           <PolarRadiusAxis />
+          <Tooltip />
           <Radar
             name="Channels"
             dataKey="messages"
