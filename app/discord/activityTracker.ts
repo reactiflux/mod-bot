2eb7afbbf20e5aa5ea20febade5c712609287e91
DanielFGray <danielfgray@gmail.com>
2025-06-10 16:34:43 -0500

    move message parsing features into separate file

--- app/discord/activityTracker.ts
+++ app/discord/activityTracker.ts
@@ -1,6 +1,11 @@
 import { Events, ChannelType } from "discord.js";
 import type { Client, Message, PartialMessage, TextChannel } from "discord.js";
 import db from "#~/db.server";
+import {
+  parseMarkdownBlocks,
+  getChars,
+  getWords,
+} from "#~/helpers/messageParsing";
 
 export async function startActivityTracking(client: Client) {
   const channelCache = new Map<string, TextChannel>();
@@ -134,66 +139,4 @@ export async function reportByGuild(guildId: string) {
     .groupBy("author_id")
     .execute();
   return result;
-}
-
-// this is better than string.split(/\s+/) because it counts emojis as 1 word
-// and we can easily filter them, works much better in other languages too
-function getWords(content: string) {
-  return Array.from(
-    new Intl.Segmenter("en-us", { granularity: "word" }).segment(content),
-  ).filter((seg) => seg.isWordLike);
-}
-
-// string.split(/\s+/) will count most emojis as 2+ chars
-// this will count them as 1
-function getChars(content: string) {
-  return Array.from(
-    new Intl.Segmenter("en-us", { granularity: "grapheme" }).segment(content),
-  );
-}
-
-type MarkdownBlocks =
-  | { type: "fenced"; lang: undefined | string; content: string[] }
-  | { type: "inline"; content: string }
-  | { type: "text"; content: string };
-
-export function parseMarkdownBlocks(content: string): MarkdownBlocks[] {
-  const blocks: MarkdownBlocks[] = [];
-  // track string position
-  let idx = 0;
-
-  // replaceAll gives easy access to the position of the match
-  content.replaceAll(/```[\s\S]+?\n^```|`.+?`/gm, (match, position) => {
-    // code blocks may be preceded by text, add those first
-    const prev = content.slice(idx, position);
-    idx = position;
-    if (prev) blocks.push({ type: "text", content: prev });
-
-    // split the code block into lines for easier processing
-    const codeText = content.slice(idx, idx + match.length).split("\n");
-    // if first line starts with triple backticks, it's a fenced code block
-    if (codeText[0].startsWith("```")) {
-      // everything after backticks is language specifier
-      const lang = codeText[0].slice(3) || undefined;
-      const content = codeText.slice(1, -1); // strip fenced backticks
-      blocks.push({ type: "fenced", lang, content });
-    } else {
-      // assume its inline code, return a single string without backticks
-      const content = codeText.join("\n").slice(1, -1);
-      blocks.push({ type: "inline", content });
-    }
-
-    // update our index to the end of the match
-    idx += match.length;
-
-    // this is only here to appease TS, value is unused
-    return "";
-  });
-
-  // after processing all code blocks, there may be text left
-  if (idx < content.length) {
-    blocks.push({ type: "text", content: content.slice(idx) });
-  }
-
-  return blocks;
 }
---
+++ app/helpers/messageParsing.ts
@@ -0,0 +1,61 @@
+export type MarkdownBlocks =
+  | { type: "fenced"; lang: undefined | string; content: string[] }
+  | { type: "inline"; content: string }
+  | { type: "text"; content: string };
+
+export function parseMarkdownBlocks(content: string): MarkdownBlocks[] {
+  const blocks: MarkdownBlocks[] = [];
+  // track string position
+  let idx = 0;
+
+  // replaceAll gives easy access to the position of the match
+  content.replaceAll(/```[\s\S]+?\n^```|`.+?`/gm, (match, position) => {
+    // code blocks may be preceded by text, add those first
+    const prev = content.slice(idx, position);
+    idx = position;
+    if (prev) blocks.push({ type: "text", content: prev });
+
+    // split the code block into lines for easier processing
+    const codeText = content.slice(idx, idx + match.length).split("\n");
+    // if first line starts with triple backticks, it's a fenced code block
+    if (codeText[0].startsWith("```")) {
+      // everything after backticks is language specifier
+      const lang = codeText[0].slice(3) || undefined;
+      const content = codeText.slice(1, -1); // strip fenced backticks
+      blocks.push({ type: "fenced", lang, content });
+    } else {
+      // must be inline code, return a single string without backticks
+      const content = codeText.join("\n").slice(1, -1);
+      blocks.push({ type: "inline", content });
+    }
+
+    // update our index to the end of the match
+    idx += match.length;
+
+    // this is only here to appease TS, value is unused
+    return "";
+  });
+
+  // after processing all code blocks, there may be text left
+  if (idx < content.length) {
+    blocks.push({ type: "text", content: content.slice(idx) });
+  }
+
+  return blocks;
+}
+
+// this is better than string.split(/\s+/) because it counts emojis as 1 word
+// and we can easily filter them, works much better in other languages too
+export function getWords(content: string) {
+  return Array.from(
+    new Intl.Segmenter("en-us", { granularity: "word" }).segment(content),
+  ).filter((seg) => seg.isWordLike);
+}
+
+// string.split(/\s+/) will count most emojis as 2+ chars
+// this will count them as 1
+export function getChars(content: string) {
+  return Array.from(
+    new Intl.Segmenter("en-us", { granularity: "grapheme" }).segment(content),
+  );
+}
