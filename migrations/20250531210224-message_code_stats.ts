7de9102d2da99bae73049f82dfed32ec6527c62d
Carl Vitullo <vcarl@users.noreply.github.com>
2025-06-17 12:22:21 -0400

    Update migrations/20250531210224-message_code_stats.ts

--- migrations/20250531210224-message_code_stats.ts
+++ migrations/20250531210224-message_code_stats.ts
@@ -5,6 +5,12 @@ const column = "code_stats";
 export async function up(db: Kysely<any>) {
   return await db.schema
     .alterTable("message_stats")
+    // {
+    //   chars: number
+    //   words: number
+    //   lines: number
+    //   lang: string | undefined
+    // }[]
     .addColumn(column, "text", (c) => c.notNull().defaultTo("[]"))
     .execute();
 }
