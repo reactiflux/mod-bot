ba61352c89ec7bc60a59571df9d9621fbc558bc9
Carl Vitullo <vcarl@users.noreply.github.com>
2025-06-19 01:05:29 -0400

    Add fancier login screen (#145)
    It's just nicer
    
    <img width="984" alt="Screenshot 2025-06-19 at 12 49 13 AM"
    src="https://github.com/user-attachments/assets/7a3e252d-8d79-458b-a072-69a7fabd7936"
    />

---
+++ app/features/ServerOverview/ServerOverview.tsx
@@ -0,0 +1,13 @@
+import { Logout } from "#~/basics/logout";
+
+export function ServerOverview() {
+  return (
+    <main className="">
+      <nav>
+        <Logout />
+      </nav>
+      <section></section>
+      <footer></footer>
+    </main>
+  );
+}
