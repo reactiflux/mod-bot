e60067c33c58a46d91540bf2a4d90123bfb4bc98
Carl Vitullo <carl.vitullo@gmail.com>
2025-06-19 00:48:22 -0400

    Add fancier login screen

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,55 +1,71 @@
 import { Login } from "#~/components/login";
-import { Logout } from "#~/components/logout";
+import { ServerOverview } from "#~/features/ServerOverview";
 
 import { useOptionalUser } from "#~/utils";
 
+const EmojiBackdrop = () => {
+  return (
+    <div
+      className="absolute inset-0 origin-center rotate-12"
+      style={{
+        width: "300%",
+        height: "300%",
+        top: "-100%",
+        left: "-100%",
+      }}
+    >
+      <div
+        className="w-full h-full animate-slide bg-yellow-500 bg-opacity-50"
+        style={{
+          backgroundBlendMode: "color",
+          backgroundSize: "300px 150px",
+          backgroundRepeat: "repeat",
+          backgroundImage: `url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 100" width="200" height="100"><text x="0" y="45" font-size="50">🧑‍⚖️⚖️📜👀</text><text x="0" y="93" font-size="50">📜🧑‍⚖️👀⚖️</text></svg>')`,
+        }}
+      />
+      <style>{`
+        @keyframes slide {
+          0% {
+            transform: translateX(0);
+          }
+          100% {
+            transform: translateX(-300px);
+          }
+        }
+        .animate-slide {
+          animation: slide 20s linear infinite;
+        }
+      `}</style>
+    </div>
+  );
+};
+
 export default function Index() {
   const user = useOptionalUser();
-  return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      <div className="relative sm:pb-16 sm:pt-8">
-        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
-            <div className="absolute inset-0">
-              <img
-                className="h-full w-full object-cover"
-                src="https://user-images.githubusercontent.com/1500684/157774694-99820c51-8165-4908-a031-34fc371ac0d6.jpg"
-                alt="Sonic Youth On Stage"
-              />
-              <div className="absolute inset-0 bg-[color:rgba(254,204,27,0.5)] mix-blend-multiply" />
-            </div>
 
-            <div className="lg:pb-18 relative w-full max-w-xl px-4 pb-8 pt-16 sm:px-6 sm:pb-14 sm:pt-24 lg:px-8 lg:pt-32">
-              <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
-                <span className="block uppercase text-yellow-500 drop-shadow-md">
-                  Euno
-                </span>
-              </h1>
-              <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
-                {user ? (
-                  <Logout />
-                ) : (
-                  <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
-                    <Login>Log in</Login>
-                  </div>
-                )}
+  if (!user) {
+    return (
+      <main className="relative min-h-screen bg-white flex items-center justify-center overflow-hidden">
+        <EmojiBackdrop />
+        <div className="relative pb-16 pt-8">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="relative shadow-xl overflow-hidden rounded-2xl bg-white">
+              <div className="lg:pb-18 relative w-full max-w-xl px-6 pb-14 pt-24 lg:px-8 lg:pt-24">
+                <h1 className="text-center text-9xl font-extrabold tracking-tight mb-10">
+                  <span className="block uppercase text-yellow-500 drop-shadow-md">
+                    Euno
+                  </span>
+                </h1>
+                <p className="text-slate-800">
+                  A community-in-a-box bot for large Discord servers
+                </p>
+                <Login>Log in</Login>
               </div>
-              <p className="mx-auto mt-6 max-w-md text-center text-xl text-white">
-                This is a development placeholder for Euno, a Discord moderation
-                bot.
-              </p>
-              <p className="mx-auto mt-6 max-w-md text-center text-xl text-white drop-shadow-md">
-                Coming soon:
-                <ul>
-                  <li>ticketing??</li>
-                  <li>activity reports</li>
-                  <li>other fun things</li>
-                </ul>
-              </p>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
+  return <ServerOverview />;
 }
