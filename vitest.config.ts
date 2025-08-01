186ffcc84dfb11015eee0b61cd724a1ea443593a
Carl Vitullo <carl.vitullo@gmail.com>
2025-08-01 01:35:32 -0400

    Fix e2e tests running under vite

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
+    exclude: ["node_modules", "tests/e2e/**"],
     globals: true,
     environment: "happy-dom",
     setupFiles: ["./test/setup-test-env.ts"],
