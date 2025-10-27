16987631210e8358a3da95d8d7bba4977d8bd3d9
Carl Vitullo <vcarl@users.noreply.github.com>
2025-10-27 15:22:31 -0400

    Add posthog (#188)
    - [x] needs new secrets configured before ready to merge

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,18 @@
 import { HydratedRouter } from "react-router/dom";
 import { hydrateRoot } from "react-dom/client";
+import { PostHogProvider } from "posthog-js/react";
 
-hydrateRoot(document, <HydratedRouter />);
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: "2025-05-24" as const,
+};
+
+hydrateRoot(
+  document,
+  <PostHogProvider
+    apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+    options={options}
+  >
+    <HydratedRouter />
+  </PostHogProvider>,
+);
