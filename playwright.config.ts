3be34a4ef2a267f1f4060d73fa70f3b0e3629160
Carl Vitullo <carl.vitullo@gmail.com>
2025-11-19 20:14:02 -0500

    prod build for e2e ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: "npm run dev",
+    command: "npm run build; npm start",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
     timeout: 120000,
