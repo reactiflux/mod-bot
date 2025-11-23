67ac7131137195a5a9553d083c8c47e1a41defee
Jernik <git@lukemiller.me>
2025-11-23 12:41:27 -0500

    made some tweaks for first time setup issues

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,5 +1,8 @@
 import type { Config } from "@react-router/dev/config";
+<<<<<<< HEAD
 
+=======
+>>>>>>> 3a3981e (made some tweaks for first time setup issues)
 export default {
   ssr: true,
 } satisfies Config;
