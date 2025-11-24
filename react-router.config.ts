28d5fd2abc20016b7a832d756ba7af9038314d31
Jernik <git@lukemiller.me>
2025-11-24 10:28:00 -0500

    fix rebase

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,5 @@
 import type { Config } from "@react-router/dev/config";
-<<<<<<< HEAD
 
-=======
->>>>>>> 3a3981e (made some tweaks for first time setup issues)
 export default {
   ssr: true,
 } satisfies Config;
