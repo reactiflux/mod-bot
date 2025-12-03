ad3e4cad5120275ee4603a248debbd72aae5bc7c
Carl Vitullo <carl.vitullo@gmail.com>
2025-12-03 00:19:21 -0500

    Delete now dead hooks from Reacord

--- app/helpers/modResponse.ts
+++ app/helpers/modResponse.ts
@@ -1,6 +1,3 @@
-import { uniq } from "lodash-es";
-import { useCallback, useMemo, useState } from "react";
-
 export const resolutions = {
   track: "track",
   // warning: "formalWarning",
@@ -18,41 +15,3 @@ export const humanReadableResolutions = {
   [resolutions.ban]: "Ban",
 } as const;
 export type Resolution = (typeof resolutions)[keyof typeof resolutions];
-
-export const useVotes = () => {
-  const [votes, setVotes] = useState({} as Record<Resolution, string[]>);
-  const recordVote = useCallback(
-    (oldVotes: typeof votes, newVote: Resolution, userId: string) => {
-      const newVotes = {
-        ...oldVotes,
-        [newVote]: uniq((oldVotes[newVote] ?? []).concat(userId)),
-      };
-      setVotes(newVotes);
-
-      return Object.entries(newVotes).reduce(
-        (accum, [resolution, voters]) => {
-          if (voters.length > accum.voteCount) {
-            // Boooo this cast because .entries() doesn't save key types
-            accum.leader = resolution as Resolution;
-            accum.voteCount = voters.length;
-            // TODO: account for ties. actually nah just require odd number of votes
-          }
-          return accum;
-        },
-        { leader: undefined, voteCount: 0 } as {
-          leader?: Resolution;
-          voteCount: number;
-        },
-      );
-    },
-    [],
-  );
-
-  return useMemo(
-    () => ({
-      recordVote,
-      votes,
-    }),
-    [votes, recordVote],
-  );
-};
