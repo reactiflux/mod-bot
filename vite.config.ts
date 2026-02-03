bad2febab2a2f4f79fa4d4845dfa4b0d91b92953
Carl Vitullo <carl.vitullo@gmail.com>
2026-02-03 17:12:38 -0500

    Fix top-level async/await by explicitly targeting esnext on the server

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { reactRouter } from "@react-router/dev/vite";
 
 export default defineConfig(({ isSsrBuild }) => ({
   build: {
+    target: isSsrBuild ? "esnext" : undefined,
     sourcemap: true,
     rollupOptions: isSsrBuild ? { input: "./app/server.ts" } : undefined,
   },
